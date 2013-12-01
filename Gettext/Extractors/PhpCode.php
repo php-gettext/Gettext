093f4b9519e4c59cf20303a9b28b9e7e91044396
Óscar Otero <oom@oscarotero.com>
2013-12-01 14:48:50 +0100

    Improvements in PhpCode extractor

--- Gettext/Extractors/PhpCode.php
+++ Gettext/Extractors/PhpCode.php
@@ -15,20 +15,22 @@ class PhpCode extends Extractor {
 
 	static public function parse ($file, Entries $entries) {
 		$tokens = token_get_all(file_get_contents($file));
+		$count = count($tokens);
 		$functions = array();
-		$currentFunction = null;
+		$bufferFunctions = array();
 
-		foreach ($tokens as $k => $value) {
+		for ($k = 0; $k < $count; $k++) {
+			$value = $tokens[$k];
+
 			if (is_string($value)) {
-				if ($value === ')' && $currentFunction) {
-					$functions[] = $currentFunction;
-					$currentFunction = null;
+				if ($value === ')' && isset($bufferFunctions[0])) {
+					$functions[] = array_shift($bufferFunctions);
 				}
 
 				continue;
 			}
 
-			if ($currentFunction && ($value[0] === T_CONSTANT_ENCAPSED_STRING)) {
+			if (isset($bufferFunctions[0]) && ($value[0] === T_CONSTANT_ENCAPSED_STRING)) {
 				$val = $value[1];
 
 				if ($val[0] === '"') {
@@ -37,12 +39,13 @@ class PhpCode extends Extractor {
 					$val = str_replace("\\'", "'", $val);
 				}
 
-				$currentFunction[] = substr($val, 1, -1);
+				$bufferFunctions[0][] = substr($val, 1, -1);
 				continue;
 			}
 
-			if (!$currentFunction && ($value[0] === T_STRING) && is_string($tokens[$k + 1]) && ($tokens[$k + 1] === '(')) {
-				$currentFunction = array($value[1], $value[2]);
+			if (($value[0] === T_STRING) && is_string($tokens[$k + 1]) && ($tokens[$k + 1] === '(')) {
+				array_unshift($bufferFunctions, array($value[1], $value[2]));
+				$k++;
 				continue;
 			}
 		}
