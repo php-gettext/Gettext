0ff45047aae292ff089727b92f54b6708fecf3bf
Lito <lito@eordes.com>
2014-11-19 12:14:17 +0100

    Added Blade templating engine extractor

---
+++ src/Extractors/Blade.php
@@ -0,0 +1,22 @@
+<?php
+namespace Gettext\Extractors;
+
+use Gettext\Translations;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\View\Compilers\BladeCompiler;
+
+/**
+ * Class to get gettext strings from blade.php files returning arrays
+ */
+class Blade extends Extractor implements ExtractorInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public static function fromString($string, Translations $translations = null, $file = '')
+    {
+        $string = (new BladeCompiler(new Filesystem, null))->compileString($string);
+
+        return PhpCode::fromString($string, $translations, $file);
+    }
+}
