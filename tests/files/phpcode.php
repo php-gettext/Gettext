18f499e969c1ccf7f49cfa5be2a0ff8c5a40b0e5
oscarotero <oom@oscarotero.com>
2015-01-06 19:03:03 +0100

    fixed test for php5.3

--- tests/files/phpcode.php
+++ tests/files/phpcode.php
@@ -16,7 +16,7 @@
     <p><?php __("text 7 (with parenthesis)"); ?></p>
     <p><?php __("text 8 \"with escaped double quotes\""); ?></p>
     <p><?php __("text 9 'with single quotes'"); ?></p>
-    <p><?= n__("text 10 with plural", "The plural form", 5); ?></p>
+    <p><?php echo n__("text 10 with plural", "The plural form", 5); ?></p>
 </div>
 
 <?php __e("<div id=\"blog\" class=\"container\">
