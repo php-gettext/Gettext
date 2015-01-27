47b66b54aeeb1a6e2fa884b6f2dc872f9fd10fe3
oscarotero <oom@oscarotero.com>
2015-01-27 21:53:10 +0100

    added JsonDictionary extractor

---
+++ src/Extractors/JsonDictionary.php
@@ -0,0 +1,28 @@
+<?php
+namespace Gettext\Extractors;
+
+use Gettext\Translations;
+
+/**
+ * Class to get gettext strings from plain json
+ */
+class JsonDictionary extends Extractor implements ExtractorInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public static function fromString($string, Translations $translations = null, $file = '')
+    {
+        if ($translations === null) {
+            $translations = new Translations();
+        }
+
+        if (($entries = json_decode($string, true))) {
+            foreach ($entries as $original => $translation) {
+                $translations->insert(null, $original)->setTranslation($translation);
+            }
+        }
+
+        return $translations;
+    }
+}
---
+++ tests/JsonDictionaryExtractorTest.php
@@ -0,0 +1,21 @@
+<?php
+include_once dirname(__DIR__).'/src/autoloader.php';
+
+class JsonDictionaryExtractorTest extends PHPUnit_Framework_TestCase
+{
+    public function testGeneration()
+    {
+        $string = '{"apple": "maza", "water": "auga"}';
+
+        //Extract translations
+        $translations = Gettext\Translations::fromJsonDictionaryString($string);
+
+        $this->assertInstanceOf('Gettext\\Translations', $translations);
+        $this->assertEquals(2, count($translations));
+
+        $translation = $translations->find('', 'water');
+
+        $this->assertInstanceOf('Gettext\\Translation', $translation);
+        $this->assertEquals('auga', $translation->getTranslation());
+    }
+}
