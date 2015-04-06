ab55a04e1893eff654a04510c75b79547fe2299f
oscarotero <oom@oscarotero.com>
2015-04-06 11:35:33 +0200

    php-cs-fixer

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,11 +4,11 @@ error_reporting(E_ALL);
 $path = dirname(__DIR__);
 
 if (is_file($path.'/vendor/autoload.php')) {
-	include_once $path.'/vendor/autoload.php';
+    include_once $path.'/vendor/autoload.php';
 } elseif (is_file($path.'/../../vendor/autoload.php')) {
-	include_once $path.'/../../vendor/autoload.php';
+    include_once $path.'/../../vendor/autoload.php';
 } else {
-	throw new \Exception("Composer autoloader not found! ($path)");
+    throw new \Exception("Composer autoloader not found! ($path)");
 }
 
 PHPUnit_Framework_Error_Notice::$enabled = true;
