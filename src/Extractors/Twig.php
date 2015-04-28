218b3d0e47bd1264d14acc168526cc88aeb5133c
Adam McCann <adam.mccann@videogamer.com>
2015-04-28 16:02:33 +0100

    Modified Twig extractor to allow user to add custom Twig extensions should they be required to parse templates for gettext

--- src/Extractors/Twig.php
+++ src/Extractors/Twig.php
@@ -16,18 +16,65 @@ class Twig extends Extractor implements ExtractorInterface
      */
     public static function fromString($string, Translations $translations = null, $file = '')
     {
-        $twigCompiler = new Twig_Loader_String();
-        $twig = new Twig_Environment($twigCompiler);
+        self::addExtension('Twig_Extensions_Extension_I18n');
 
-        $twig->addExtension(new Twig_Extensions_Extension_I18n());
+        $string = self::$twig->compileSource($string);
 
         // add default global php gettext functions
         PhpCode::$functions['gettext'] = '__';
         PhpCode::$functions['ngettext'] = '__';
         PhpCode::$functions['_'] = '__';
 
-        $string = $twig->compileSource($string);
-
         return PhpCode::fromString($string, $translations, $file);
     }
+
+    /**
+     * Initialise Twig if it isn't already, and add a given Twig extension.
+     * This must be called before calling fromString()
+     *
+     * @param mixed Already initialised extension to add
+     */
+    public static function addExtension($extension)
+    {
+        // Get an instance of the extension
+        // Support for string, closure and an object
+        /*
+        if (is_string($twig_extension)) {
+            $twig_extension = new $twig_extension($this->app, $twig);
+        } elseif (is_callable($twig_extension)) {
+            $twig_extension = $twig_extension($this->app, $twig);
+        } elseif (!is_object($twig_extension)) {
+            throw new InvalidArgumentException('Incorrect extension type');
+        }
+        */
+
+        // initialise twig
+        if (!isset(self::$twig)) {
+            $twigCompiler = new Twig_Loader_String();
+
+            self::$twig = new Twig_Environment($twigCompiler);
+        }
+
+        if (!self::checkHasExtensionByClassName($extension)) {
+            self::$twig->addExtension(new $extension());
+        }
+    }
+
+    /**
+     * Checks if a given Twig extension is already registered or not
+     *
+     * @param  string   Name of Twig extension to check
+     * @return boolean  Whether it has been registered already or not
+     */
+    protected static function checkHasExtensionByClassName($className)
+    {
+        foreach (self::$twig->getExtensions() as $extension) {
+            if ($className == get_class($extension)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
 }
