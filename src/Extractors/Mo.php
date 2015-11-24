0fcd8e66e8daa4061f2d85c1217bcbd62dafe9b9
Lito <lito@eordes.com>
2015-11-24 14:24:53 +0100

    Improved code quality

--- src/Extractors/Mo.php
+++ src/Extractors/Mo.php
@@ -1,7 +1,7 @@
 <?php
-
 namespace Gettext\Extractors;
 
+use Exception;
 use Gettext\Translations;
 use Gettext\Utils\StringReader;
 
@@ -31,7 +31,7 @@ class Mo extends Extractor implements ExtractorInterface
         } elseif ($magic === (self::MAGIC2 & 0xFFFFFFFF)) {
             $byteOrder = 'N'; //big endian
         } else {
-            throw new \Exception('Not MO file');
+            throw new Exception('Not MO file');
         }
 
         self::readInt($stream, $byteOrder);
@@ -42,56 +42,67 @@ class Mo extends Extractor implements ExtractorInterface
 
         $stream->seekto($originals);
         $table_originals = self::readIntArray($stream, $byteOrder, $total * 2);
+
         $stream->seekto($tran);
         $table_translations = self::readIntArray($stream, $byteOrder, $total * 2);
 
         for ($i = 0; $i < $total; ++$i) {
-            $stream->seekto($table_originals[$i * 2 + 2]);
-            $original = $stream->read($table_originals[$i * 2 + 1]);
-            $stream->seekto($table_translations[$i * 2 + 2]);
-            $translated = $stream->read($table_translations[$i * 2 + 1]);
+            $next = $i * 2;
+
+            $stream->seekto($table_originals[$next + 2]);
+            $original = $stream->read($table_originals[$next + 1]);
+
+            $stream->seekto($table_translations[$next + 2]);
+            $translated = $stream->read($table_translations[$next + 1]);
+
+            if ($translated === '') {
+                continue;
+            }
 
             if ($original === '') {
                 // Headers
                 foreach (explode("\n", $translated) as $headerLine) {
-                    if ($headerLine !== '') {
-                        $headerChunks = preg_split('/:\s*/', $headerLine, 2);
-                        $translations->setHeader($headerChunks[0], isset($headerChunks[1]) ? $headerChunks[1] : '');
+                    if ($headerLine === '') {
+                        continue;
                     }
+
+                    $headerChunks = preg_split('/:\s*/', $headerLine, 2);
+                    $translations->setHeader($headerChunks[0], isset($headerChunks[1]) ? $headerChunks[1] : '');
                 }
+
+                continue;
+            }
+
+            $chunks = explode("\x04", $original, 2);
+
+            if (isset($chunks[1])) {
+                $context = $chunks[0];
+                $original = $chunks[1];
             } else {
-                $chunks = explode("\x04", $original, 2);
+                $context = '';
+            }
 
-                if (isset($chunks[1])) {
-                    $context = $chunks[0];
-                    $original = $chunks[1];
+            $chunks = explode("\x00", $original, 2);
+
+            if (isset($chunks[1])) {
+                $original = $chunks[0];
+                $plural = $chunks[1];
+            } else {
+                $plural = '';
+            }
+
+            $translation = $translations->insert($context, $original, $plural);
+
+            if ($plural === '') {
+                $translation->setTranslation($translated);
+                continue;
+            }
+
+            foreach (explode("\x00", $translated) as $pluralIndex => $pluralValue) {
+                if ($pluralIndex === 0) {
+                    $translation->setTranslation($pluralValue);
                 } else {
-                    $context = '';
-                }
-
-                $chunks = explode("\x00", $original, 2);
-
-                if (isset($chunks[1])) {
-                    $original = $chunks[0];
-                    $plural = $chunks[1];
-                } else {
-                    $plural = '';
-                }
-
-                $translation = $translations->insert($context, $original, $plural);
-
-                if ($translated !== '') {
-                    if ($plural === '') {
-                        $translation->setTranslation($translated);
-                    } else {
-                        foreach (explode("\x00", $translated) as $pluralIndex => $pluralValue) {
-                            if ($pluralIndex === 0) {
-                                $translation->setTranslation($pluralValue);
-                            } else {
-                                $translation->setPluralTranslation($pluralValue, $pluralIndex - 1);
-                            }
-                        }
-                    }
+                    $translation->setPluralTranslation($pluralValue, $pluralIndex - 1);
                 }
             }
         }
@@ -124,3 +135,4 @@ class Mo extends Extractor implements ExtractorInterface
         return unpack($byteOrder.$count, $stream->read(4 * $count));
     }
 }
+
