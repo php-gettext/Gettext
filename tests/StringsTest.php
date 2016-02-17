75b187bbe681ab0cb90652fc42922173ea3b6ea8
Michele Locati <michele@locati.it>
2016-02-17 08:50:14 +0100

    Test for PO string conversions

--- tests/StringsTest.php
+++ tests/StringsTest.php
@@ -22,7 +22,7 @@ class StringsTest extends PHPUnit_Framework_TestCase
             array('"{$obj->name}"', '{$obj->name}'),
             array('"a\x20b $c"', 'a b $c'),
             array('"a\x01b\2 \1 \01 \001 \r \n \t \v \f"', "a\1b\2 \1 \1 \1 \r \n \t \v \f"),
-            array('"$ \$a \""', '$ $a "')
+            array('"$ \$a \""', '$ $a "'),
         );
     }
 
@@ -32,5 +32,31 @@ class StringsTest extends PHPUnit_Framework_TestCase
     public function testStringFromPhp($source, $decoded)
     {
         $this->assertSame($decoded, Strings::fromPhp($source));
+    }
+
+    public function poStringsProvider()
+    {
+        return array(
+            array('test', '"test"'),
+            array("'test'", '"\'test\'"'),
+            array("Special chars: \r \n \t", '"Special chars: \\r \\n \\t"'),
+            array("Newline\nSlash and n\\n", '"Newline\nSlash and n\\n"'),
+        );
+    }
+
+    /**
+     * @dataProvider poStringsProvider
+     */
+    public function testStringToPo($phpString, $poString)
+    {
+        $this->assertSame($poString, Strings::ToPo($phpString));
+    }
+
+    /**
+     * @dataProvider poStringsProvider
+     */
+    public function testStringFromPo($phpString, $poString)
+    {
+        $this->assertSame($phpString, Strings::fromPo($poString));
     }
 }
