9733ab7a863f515215fadbf7236d1f96f5a974df
Michele Locati <michele@locati.it>
2016-02-18 11:08:13 +0100

    Test extraction of variables and concatenated strings

--- tests/files/special-chars.php
+++ tests/files/special-chars.php
@@ -1,7 +1,15 @@
 <div>
-	<p><?php __('plain'); ?></p>
+    <p><?php __('plain'); ?></p>
     <p><?php __('DATE \a\t TIME'); ?></p>
     <p><?php __("DATE \a\\t TIME"); ?></p>
     <p><?php __("DATE \\a\\t TIME"); ?></p>
     <p><?php __("FIELD\tFIELD"); ?></p>
+    <p><?php __(
+        "text "
+        // test
+        .'concatenated'.
+        /* test*/ " with 'comments'"
+    ); ?></p>
+    <p><?php __($avoid['me']); ?>
+    <p><?php __('Stop at the variable'.$var.'!'); ?>
 </div>
