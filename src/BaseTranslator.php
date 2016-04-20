7f845310586ae26c8a5a84fa5a9c284b1afbf356
oscarotero <oom@oscarotero.com>
2016-04-20 21:01:50 +0200

    removed Translator::initGettextFunctions

--- src/BaseTranslator.php
+++ src/BaseTranslator.php
@@ -8,22 +8,12 @@ abstract class BaseTranslator implements TranslatorInterface
     public static $current;
 
     /**
-     * Set a translation instance as global, to use it with the gettext functions.
-     *
-     * @param TranslatorInterface $translator
-     */
-    public static function initGettextFunctions(TranslatorInterface $translator)
-    {
-        self::$current = $translator;
-
-        include_once __DIR__.'/translator_functions.php';
-    }
-
-    /**
      * @see TranslatorInterface
      */
     public function register()
     {
-        self::initGettextFunctions($this);
+        self::$current = $this;
+
+        include_once __DIR__.'/translator_functions.php';
     }
 }
