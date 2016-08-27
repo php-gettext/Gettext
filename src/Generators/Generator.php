b00da35e5966fa88c7d8c09909152ebadf14f6b4
Alexander Günsche <mail@lxg.de>
2016-08-27 09:13:11 +0200

    pass options array to toString method

--- src/Generators/Generator.php
+++ src/Generators/Generator.php
@@ -11,7 +11,7 @@ abstract class Generator implements GeneratorInterface
      */
     public static function toFile(Translations $translations, $file, array $options = [])
     {
-        $content = static::toString($translations);
+        $content = static::toString($translations, $options);
 
         if (file_put_contents($file, $content) === false) {
             return false;
