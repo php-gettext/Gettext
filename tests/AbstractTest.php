7fdb03a87410953f3b1eeda31b0fe65e39d9defb
peter279k <peter279k@gmail.com>
2018-04-24 16:18:47 +0800

    Test enhancement

--- tests/AbstractTest.php
+++ tests/AbstractTest.php
@@ -3,9 +3,9 @@
 namespace Gettext\Tests;
 
 use Gettext\Translations;
-use PHPUnit_Framework_TestCase;
+use PHPUnit\Framework\TestCase;
 
-abstract class AbstractTest extends PHPUnit_Framework_TestCase
+abstract class AbstractTest extends TestCase
 {
     protected static $ext = [
         'Blade' => 'php',
