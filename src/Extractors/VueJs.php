8e1dbc393dd459de18aa7787e01d94ad941e0429
Quentin Harnay <qharnay@sellsy.com>
2019-07-04 10:29:22 +0200

    Add support for VueJs v-text attributes for i18n strings extract

--- src/Extractors/VueJs.php
+++ src/Extractors/VueJs.php
@@ -26,6 +26,7 @@ class VueJs extends JsCode implements ExtractorInterface
                 ':',
                 'v-bind:',
                 'v-on:',
+                'v-text',
             ],
             // HTML Tags to parse
             'tagNames' => [
