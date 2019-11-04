059ee698c5ca21339a44ecba6174b889a17846c7
Oscar Otero <oscar@marketgoo.com>
2019-11-04 19:32:41 +0100

    improved Merge bitwise constants

--- src/Merge.php
+++ src/Merge.php
@@ -8,28 +8,25 @@ namespace Gettext;
  */
 final class Merge
 {
-    const TRANSLATIONS_OURS = 1;
-    const TRANSLATIONS_THEIRS = 2;
-    const TRANSLATIONS_OVERRIDE = 4;
+    const TRANSLATIONS_OURS = 1 << 0;
+    const TRANSLATIONS_THEIRS = 1 << 1;
+    const TRANSLATIONS_OVERRIDE = 1 << 2;
 
-    const HEADERS_OURS = 8;
-    const HEADERS_THEIRS = 16;
-    const HEADERS_OVERRIDE = 32;
+    const HEADERS_OURS = 1 << 3;
+    const HEADERS_THEIRS = 1 << 4;
+    const HEADERS_OVERRIDE = 1 << 5;
 
-    const LANGUAGE_OVERRIDE = 64;
-    const DOMAIN_OVERRIDE = 128;
+    const COMMENTS_OURS = 1 << 6;
+    const COMMENTS_THEIRS = 1 << 7;
 
-    const COMMENTS_OURS = 256;
-    const COMMENTS_THEIRS = 512;
+    const EXTRACTED_COMMENTS_OURS = 1 << 8;
+    const EXTRACTED_COMMENTS_THEIRS = 1 << 9;
 
-    const EXTRACTED_COMMENTS_OURS = 1024;
-    const EXTRACTED_COMMENTS_THEIRS = 2048;
+    const FLAGS_OURS = 1 << 10;
+    const FLAGS_THEIRS = 1 << 11;
 
-    const FLAGS_OURS = 4096;
-    const FLAGS_THEIRS = 8192;
-
-    const REFERENCES_OURS = 16384;
-    const REFERENCES_THEIRS = 32768;
+    const REFERENCES_OURS = 1 << 12;
+    const REFERENCES_THEIRS = 1 << 13;
 
     //Merge strategies
     const SCAN_AND_LOAD =
